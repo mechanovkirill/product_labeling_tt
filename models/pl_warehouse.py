c02bda93af28c351b8039218174223436b43605a
mechanovkirill <mechanovkirill@gmail.com>
2024-01-10 22:39:05 +0400

    Add products to warehouse.

--- models/pl_warehouse.py
+++ models/pl_warehouse.py
@@ -6,4 +6,5 @@ class PLWarehouse(models.Model):
     _description = 'Warehouse'
 
     name = fields.Char(required=True)
+    pl_labeled_product_id = fields.One2many('product_labeling.labeled_product', 'pl_warehouse_id')
     
